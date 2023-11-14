bac26e1fe8822f713df37489bfaebff10007dff0
Eva Tavares <eva.programadora@gmail.com>
2023-11-14 16:49:25 -0300

    feat: navbar and pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <header className='header'>
-        <NavLink to='/' className=''>
-            <p>ET</p>
+        <NavLink to='/' className='w-10 h-10 rouded-lg bg-white items-center justify-center flex font-bold shadow-md'>
+            <p className='blue-gradient_text'>ET</p>
         </NavLink>
+        <nav className='flex text-lg gap-7 font-medium'>
+          <NavLink to="/about" className={({isActive}) => isActive ? 'text-black-500' : 'text-black'}>
+            About
+          </NavLink>
+          <NavLink to="/projects" className={({isActive}) => isActive ? 'text-black-500' : 'text-black'}>
+            Projects
+          </NavLink>
+        </nav>
     </header>
   )
 }

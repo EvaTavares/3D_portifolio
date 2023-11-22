ae2f2c6d5e8ecc5a57f8e78a681ee6590ed62c8d
Eva Tavares <eva.programadora@gmail.com>
2023-11-22 11:27:13 -0300

    add: comentários

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -14,18 +14,25 @@ export function Bird() {
   },[])
 
   useFrame(({clock, camera})=>{
+    // atualiza a posição vertical do movimento dopassaro usando o sine wave - sinal de ondas
     birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2
 
+    //vê se o pássaro atingiu um determinado ponto final em relação à câmera
     if(birdRef.current.position.x > camera.position.x + 10){
+      // muda a direção para voltar e a rotação de 180° no eixo Y
         birdRef.current.rotation.y = Math.PI;
     } else if(birdRef.current.position.x < camera.position.x - 10) {
+      // muda a direção para frente e recomeça a rotação do pássaro
         birdRef.current.rotation.y = 0;
     }
 
+    // atualiza as posições X e Z baseado na direção
     if(birdRef.current.rotation.y === 0 ){
+      // movimento para frente
         birdRef.current.position.x += 0.01;
         birdRef.current.position.z -= 0.01;
     } else {
+      // movimento para trás
         birdRef.current.position.x -= 0.01;
         birdRef.current.position.z += 0.01;
 
